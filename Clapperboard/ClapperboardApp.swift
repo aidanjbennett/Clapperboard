6528eb23520facb8ea9eee8614504048ea67a31b
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2026-03-22 18:08:15 +0000

    Install and run Sentry wizard

--- Clapperboard/ClapperboardApp.swift
+++ Clapperboard/ClapperboardApp.swift
@@ -6,9 +6,37 @@
 //
 
 import SwiftUI
+import Sentry
+
 
 @main
 struct ClapperboardApp: App {
+    init() {
+        SentrySDK.start { options in
+            options.dsn = "https://0e62762dba491e3da941388f48e6a958@o4509298667094016.ingest.de.sentry.io/4511089404608592"
+
+            // Adds IP for users.
+            // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
+            options.sendDefaultPii = true
+
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+
+            // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
+            options.configureProfiling = {
+                $0.sessionSampleRate = 1.0 // We recommend adjusting this value in production.
+                $0.lifecycle = .trace
+            }
+
+            // Uncomment the following lines to add more data to your events
+            // options.attachScreenshot = true // This adds a screenshot to the error events
+            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+            
+            // Enable experimental logging features
+            options.experimental.enableLogs = true
+        }
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
